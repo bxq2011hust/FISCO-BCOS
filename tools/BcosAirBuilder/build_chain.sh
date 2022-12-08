9f49bdce36c95ea05f28e4f59ed6caf7da0e8dd3
wenlinli <1574249665@qq.com>
2022-12-08 10:48:55 +0800

    fix build_chain.sh download binary error (#3215)

--- tools/BcosAirBuilder/build_chain.sh
+++ tools/BcosAirBuilder/build_chain.sh
@@ -45,6 +45,7 @@ monitor_mode="false"
 auth_admin_account=
 binary_path=""
 lightnode_binary_path=""
+download_lightnode_binary="false"
 mtail_binary_path=""
 wasm_mode="false"
 serial_mode="false"
@@ -566,6 +567,7 @@ parse_params() {
             ;;
         L)
             lightnode_binary_path="$OPTARG"
+            download_lightnode_binary="true"
             ;;
         o)
             output_dir="$OPTARG"
@@ -1696,7 +1698,7 @@ deploy_nodes()
             LOG_FATAL "fisco bcos binary exec ${binary_path} not exist, Must copy binary file ${binary_name} to ${binary_path}"
         fi
     fi
-    if [ -z "${lightnode_binary_path}" ];then
+    if [ -z "${lightnode_binary_path}" ] && [ "${download_lightnode_binary}" == "true" ];then
         download_lightnode_bin
         echo "lightnode_binary_path is ${lightnode_binary_path}"
         if [[ ! -f "$lightnode_binary_path" ]]; then
